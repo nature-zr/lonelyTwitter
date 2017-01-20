6163567ef0cd677d2fe998c6375a71e032014db9
nature-zr <nature.li@hotmail.com>
2017-01-20 15:20:23 -0700

    Update Tweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -2,10 +2,6 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-/**
- * Created by shida3 on 1/19/17.
- */
-
 public abstract class Tweet implements Tweetable{
     private Date date;
     private String message;
