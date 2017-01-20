fba8ee397185895555cc4bc278f128a30c63a43d
nature-zr <nature.li@hotmail.com>
2017-01-20 15:18:23 -0700

    Update ImportantTweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -2,10 +2,6 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-/**
- * Created by shida3 on 1/19/17.
- */
-
 public class ImportantTweet extends Tweet {
     public ImportantTweet(String message) {
         super(message);
