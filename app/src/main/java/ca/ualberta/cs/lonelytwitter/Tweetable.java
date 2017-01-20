2bf8cca4ef76b084299a3d885202b59d5b431139
nature-zr <nature.li@hotmail.com>
2017-01-20 15:21:42 -0700

    Update Tweetable.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -1,9 +1,5 @@
 package ca.ualberta.cs.lonelytwitter;
 
-/**
- * Created by shida3 on 1/19/17.
- */
-
 public interface Tweetable {
     public String getMessage();
     public void setMessage(String string) throws TweetTooLongException;
