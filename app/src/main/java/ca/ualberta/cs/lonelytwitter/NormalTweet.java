825b5f80f869ebd02bbea26c53744acdf6a6bb84
nature-zr <nature.li@hotmail.com>
2017-01-20 15:20:07 -0700

    Update NormalTweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -2,10 +2,6 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-/**
- * Created by shida3 on 1/19/17.
- */
-
 public class NormalTweet extends Tweet {
     public NormalTweet(String message) {
         super(message);
